eb981b31f381029feadb833ec536cfe84fdd286f
Igor <in.lermin@gmail.com>
2019-12-25 11:19:50 +0200

    add message and version

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main(int, char **) {
-    std::cout << "Version" << version() << std::endl;
+    std::cout << "Hello, World!\n" << "Version" << version() << std::endl;
 
     return 0;
 }
