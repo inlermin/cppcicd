6b722377b99ee3fb595df92c96783213b3cb85ce
Igor <in.lermin@gmail.com>
2019-12-29 19:43:18 +0200

    added gtest and removed gitignore

--- g_test_version.cpp
+++ g_test_version.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include "lib.h"
 
 #include "gtest/gtest.h"
 
-TEST(g_test_version, test_valid_version)
-{
+#include "lib.h"
+
+TEST(g_test_version, test_valid_version) {
     EXPECT_TRUE(version() > 0);
 }
