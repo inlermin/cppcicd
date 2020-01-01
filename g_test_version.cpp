605f186c890fae4a90e9f235e317251a6f367fc6
Igor <in.lermin@gmail.com>
2020-01-01 11:04:46 +0200

    gtest add main func

--- g_test_version.cpp
+++ g_test_version.cpp
@@ -7,3 +7,9 @@
 TEST(g_test_version, test_valid_version) {
     EXPECT_TRUE(version() > 0);
 }
+
+// just for running g_test_version.exe
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
